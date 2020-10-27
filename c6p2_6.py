1645e39226d40147bdcec67762338bfd5933f942
RyanFatsena <71627584+RyanFatsena@users.noreply.github.com>
2020-10-28 00:41:19 +0700

    Create c6p2_6.py

---
+++ c6p2_6.py
@@ -0,0 +1,9 @@
+def luasSegitiga(a,t):
+    luas = a*t/2
+    return luas
+
+alas2 = 15
+tinggi2 = 45
+print('Luas segitiga dg alas', alas2,
+      'dan tinggi', tinggi2,
+      'adalah', luasSegitiga(alas2,tinggi2))
